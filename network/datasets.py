1340a5c580a161ba0c85d59504e87e4b4e40f233
Lucas Beveridge <lucasbeveridge02@gmail.com>
2022-10-20 09:16:11 +0100

    Added new Dataset class to pair sinogram windows

--- network/datasets.py
+++ network/datasets.py
@@ -7,6 +7,7 @@ from simulator.data_io import loadTiff
 class BaseDataset(Dataset):
     """Dataset to get sinograms from directories (provided data was generated according to the scripts in ../simulators)
     Data is split into train:validate:test according to parameter `tvt`, a tuple of the ratios"""
+
     def __init__(self, root, transform, mode, tvt, size=256, shifts=5):
         self.root = root
         self.size = size
@@ -19,7 +20,7 @@ class BaseDataset(Dataset):
         num_validate = int((tvt[1] / sum(tvt)) * len(self.all_datasets))
         num_test = int((tvt[2] / sum(tvt)) * len(self.all_datasets))
         self.train_datasets = self.all_datasets[:num_train]
-        self.validate_datasets = self.all_datasets[num_train:num_train+num_validate]
+        self.validate_datasets = self.all_datasets[num_train:num_train + num_validate]
         self.test_datasets = self.all_datasets[-num_test:]
         # Set current dataset to the dataset that corresponds to mode
         self.mode, self.datasets, self.filepaths = None, [], []
@@ -82,17 +83,17 @@ class BaseDataset(Dataset):
 
 class WindowDataset(BaseDataset):
     def __init__(self, root, transform, mode, tvt, windowWidth, size=256, shifts=5):
-        super().__init__(root, transform, mode, tvt, size=256, shifts=5)
+        super().__init__(root, transform, mode, tvt, size=size, shifts=shifts)
         self.windowWidth = windowWidth
 
     def getWindows(self, image):
         num_windows = image.shape[1] // self.windowWidth
         windows = []
-        for i in range(num_windows+1):
+        for i in range(num_windows + 1):
             try:
-                window = image[..., i*self.windowWidth:(i+1)*self.windowWidth]
+                window = image[..., i * self.windowWidth:(i + 1) * self.windowWidth]
             except IndexError:
-                window = image[..., i*self.windowWidth:]
+                window = image[..., i * self.windowWidth:]
             windows.append(window)
         return windows
 
@@ -110,3 +111,45 @@ class WindowDataset(BaseDataset):
     @staticmethod
     def combineWindows(window_list):
         return np.concatenate(window_list, axis=1)
+
+
+class PairedWindowDataset(WindowDataset):
+    def __init__(self, root, transform, mode, tvt, windowWidth, size=256, shifts=5):
+        super().__init__(root, transform, mode, tvt, windowWidth, size=size, shifts=shifts)
+
+    def __getitem__(self, item):
+        clean, *shifts = super().__getitem__(item)
+        centre = shifts[self.shifts // 2]
+
+        # to get best of both, loop through each shift and use one with lowest tv
+        bob = []
+        # loop through each window
+        for i in range(len(centre)):
+            centre_tv = self.gradient_sum_tv(centre[i])
+            min_tv = centre_tv
+            min_idx = self.shifts // 2  # centre index
+            # loop through each shift
+            for j in range(self.shifts):
+                shift_tv = self.gradient_sum_tv(shifts[j][i])
+                if shift_tv < min_tv:
+                    min_tv = shift_tv
+                    min_idx = j
+            bob.append(shifts[min_idx][i])
+
+        # return (clean, centre, best-of-both)
+        return clean, centre, bob
+
+    def getFullSinograms(self, item):
+        clean, centre, bob = self.__getitem__(item)
+        clean_full = self.combineWindows(clean)
+        centre_full = self.combineWindows(centre)
+        bob_full = self.combineWindows(bob)
+        return clean_full, centre_full, bob_full
+
+    def gradient_sum_tv(self, data):
+        grad_sum = np.sum(np.gradient(data, axis=1), axis=0)
+        return self.total_variation(grad_sum)
+
+    @staticmethod
+    def total_variation(data):
+        return np.sum(np.abs(data[1:] - data[:-1]))
