7a0e4f2e4860e35541a4d7670a384e4b25023430
Lucas Beveridge <lucasbeveridge02@gmail.com>
2022-11-07 10:09:14 +0000

    Added function to test stripe detection metrics on reconstructions

--- network/stripe_detection.py
+++ network/stripe_detection.py
@@ -11,9 +11,9 @@ from torch.utils.data import DataLoader
 from torchvision import transforms
 from tomobar.methodsDIR import RecToolsDIR
 
-from datasets import BaseDataset, WindowDataset, PairedWindowDataset
+from datasets import BaseDataset, WindowDataset, PairedWindowDataset, PairedFullDataset
 from visualizers import MetricVisualizer, getRectools2D
-import metrics
+from metrics import *
 
 
 def compareDifferentWidths(metric, num_widths, lo, hi, size=256, num_shifts=5):
@@ -48,7 +48,7 @@ def compareDifferentWidths(metric, num_widths, lo, hi, size=256, num_shifts=5):
 def compareDifferentMetrics(metric_list, width, size=256, num_shifts=5):
     dataroot = os.path.join(os.pardir, 'data')
     length = len(metric_list) + 2
-    vis = MetricVisualizer(size, (5, len(metric_list) + 2))
+    vis = MetricVisualizer(size, (5, len(metric_list)*2 + 1))
 
     for x in range(5):
         dataset = BaseDataset(root=dataroot, mode='train', tvt=(1, 0, 0), size=size, shifts=num_shifts)
@@ -57,17 +57,19 @@ def compareDifferentMetrics(metric_list, width, size=256, num_shifts=5):
         centre_full = shifts_full[num_shifts // 2]
 
         vis.plot_recon(clean_full, f"Clean {item}")
-        vis.plot_recon(centre_full, f"Centre {item}")
+        # vis.plot_recon(centre_full, f"Centre {item}")
 
         for metric in metric_list:
             dataset = PairedWindowDataset(root=dataroot, mode='train', tvt=(1, 0, 0), windowWidth=width,
                                           stripeMetric=metric, size=size, shifts=num_shifts)
             # GET SINOGRAM OF BOB
-            _, _, bob_full = dataset.getFullSinograms(item)
+            _, stripe_full, bob_full = dataset.getFullSinograms(item * (num_shifts // 2))
             bob_full = bob_full.squeeze()
+            stripe_full = stripe_full.squeeze()
 
             # RECONSTRUCT & PLOT
-            vis.plot_recon(bob_full, title=metric.__name__)
+            vis.plot_recon(stripe_full, title=metric.__name__)
+            vis.plot_recon(bob_full, title="Target")
     plt.show()
 
 
@@ -85,13 +87,13 @@ def test_metric(stripe_metrics, test_scores, width, size=256, num_shifts=5, visu
             # create dataset
             dataset = PairedWindowDataset(root=dataroot, mode='train', tvt=(1, 0, 0), windowWidth=width,
                                           stripeMetric=stripe_metric, size=size, shifts=num_shifts)
-            # loop through each item in dataset, calculate dice coefficient between clean & bob
+            # loop through each item in dataset, calculate metric score between clean & bob
             scores = []
             for i in range(len(dataset) // 10):
-                clean, _, bob = dataset.getFullSinograms(i)
-                score = test_score(clean, bob)
+                clean, _, bob = dataset[i]
+                score = np.mean([test_score(clean_w, bob_w) for clean_w, bob_w in zip(clean, bob)])
                 scores.append(score)
-            # calculate mean of all dice coefficients
+            # calculate mean of all metric scores
             mean_score = np.mean(scores)
             print(f"Stripe Metric: {stripe_metric.__name__}, Score Metric: {test_score.__name__}, Mean: {mean_score}")
             total_scores.append(scores)
@@ -110,9 +112,51 @@ def test_metric(stripe_metrics, test_scores, width, size=256, num_shifts=5, visu
         print("Data not saved.")
 
 
+def test_metric_recon(stripe_metrics, test_scores, width, size=256, num_shifts=5, visual=True):
+    dataroot = os.path.join(os.pardir, 'data')
+    if type(stripe_metrics) != list:
+        stripe_metrics = [stripe_metrics]
+    if type(test_scores) != list:
+        test_scores = [test_scores]
+    vis = MetricVisualizer(size, (1, 1))
+    rows = []
+    for test_score in test_scores:
+        total_scores = []
+        for stripe_metric in stripe_metrics:
+            # create dataset
+            dataset = PairedFullDataset(root=dataroot, mode='train', tvt=(1, 0, 0), windowWidth=width,
+                                        stripeMetric=stripe_metric, size=size, shifts=num_shifts)
+            # loop through each item in dataset, calculate metric score between clean & bob
+            scores = []
+            for i in range(len(dataset) // 10):
+                clean, _, bob = dataset[i]
+                clean = vis.rectools.FBP(clean)
+                bob = vis.rectools.FBP(bob)
+                score = test_score(clean, bob)
+                scores.append(score)
+            # calculate mean of all metric scores
+            mean_score = np.mean(scores)
+            print(f"Stripe Metric: {stripe_metric.__name__}, Score Metric: {test_score.__name__}, Mean: {mean_score}")
+            total_scores.append(scores)
+        if visual:
+            vis.plot_box(total_scores, titles=[stripe_metric.__name__ for stripe_metric in stripe_metrics])
+            plt.show()
+
+        rows.append([np.mean(s) for s in total_scores])
+    saveCSV = input("Save data? (y/[n]): ")
+    if saveCSV == 'y':
+        with open('recon_scores.csv', 'a', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerows(rows)
+        print("Data saved to 'recon_scores.csv'")
+    else:
+        print("Data not saved.")
+
+
 def findPairs(metric, size=256, num_shifts=5, width=25):
     dataroot = os.path.join(os.pardir, 'data')
-    dataset = PairedWindowDataset(root=dataroot, mode='train', tvt=(3, 1, 1), windowWidth=25)
+    dataset = PairedWindowDataset(root=dataroot, mode='train', tvt=(3, 1, 1), size=size, shifts=num_shifts,
+                                  windowWidth=width, stripeMetric=metric)
     item = random.randint(0, len(dataset))
     print(f"item: {item}")
     clean, stripe, plain = dataset.getFullSinograms(item)
@@ -144,6 +188,7 @@ def findPairs(metric, size=256, num_shifts=5, width=25):
 
 if __name__ == '__main__':
     # compareDifferentWidths(metrics.gradient_sum_tv, 10, 0.01, 0.2)
-    # compareDifferentMetrics(metrics.all_metrics, width=18)
-    # test_metric(metrics.gradient_sum_tv, metrics.test_metrics, width=18)
-    findPairs(metrics.gradient_sum_max)
+    # compareDifferentMetrics(stripe_detection_metrics[:5], width=18)
+    # test_metric([sum_max, gradient_sum_tv], test_metrics, width=18)
+    test_metric_recon(stripe_detection_metrics, [BCELoss, histogram_intersection], width=25, visual=False)
+    # findPairs(metrics.gradient_sum_max)
