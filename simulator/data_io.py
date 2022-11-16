Corrected normalisation when saving/loading tiffs

simulator/data_io.py
@@ -3,11 +3,13 @@ import pickle
 from PIL import Image
 
 
-def rescale(data, a=0, b=1):
+def rescale(data, a=0, b=1, imin=None, imax=None):
     """Function to normalise data in range [a, b].
     Had to call it 'rescale' because Python got confused with other functions' parameters."""
-    imin = data.min()
-    imax = data.max()
+    if imin is None:
+        imin = data.min()
+    if imax is None:
+        imax = data.max()
     # if imin == imax, then the data is a constant value, and so normalising will have no effect
     # this also avoids a Divide By Zero error
     if imin == imax:
@@ -42,9 +44,13 @@ def save3DTiff(data, path, dtype=np.uint16, normalise=True):
     Image data must be in form (z, y, x) where slices are selected along axis z.
     Slices are all saved in the same directory, with names like '<path>_0000.tif'
     where the number after <path> identifies the slice of the 3D image."""
+    if normalise:
+        data = rescale(np.abs(data))
+        if dtype == np.uint16:
+            data *= 65535
     for z in range(data.shape[0]):
         filename = path + '_' + str(z).zfill(4)
-        saveTiff(data[z, ...], filename, dtype=dtype, normalise=normalise)
+        saveTiff(data[z, ...], filename, dtype=dtype, normalise=False)
     print(f"Saved .tif image to {path}.")
 
 
@@ -52,7 +58,7 @@ def loadTiff(path, dtype=np.uint16, normalise=True):
     img = Image.open(path)
     data = np.array(img, dtype=dtype)
     if normalise:
-        data = rescale(data)
+        data = rescale(data, imin=0, imax=65535)
     return data
 
 
@@ -63,5 +69,7 @@ def load3DTiff(path, shape, dtype=np.uint16, normalise=True):
     data = np.empty(shape)
     for z in range(shape[0]):
         filename = path + '_' + str(z).zfill(4) + '.tif'
-        data[z, ...] = loadTiff(filename, dtype=dtype, normalise=normalise)
+        data[z, ...] = loadTiff(filename, dtype=dtype, normalise=False)
+    if normalise:
+        data = rescale(data, 0, 1)
     return data