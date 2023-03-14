464869231aeb984771254a87b482c5b83a88463d
Lucas Beveridge <lucasbeveridge02@gmail.com>
2023-03-14 10:22:59 +0000

    Updated train/test script parameter descriptions

--- run_scripts/train_test.sh
+++ run_scripts/train_test.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
-# Parameters
-root="./data" # root where data was created
-shifts=5 # number of shifts in vertical height of each sample
-size=256 # grid size of sample (and therefore also height of 3D sinogram generated)
-savedir="./images" # directory in which to save the models during training
-model="mask" # model type. must be one of 'window' or 'base'
-width=25 # width of windows
+# Parameters (for full description of each parameter, see ../network/README.md)
+root="./data" # directory where input data is stored
+model="mask" # type of model to train. must be one of 'base', 'mask', 'simple', 'window' or 'full'
+size=256 # number of sinograms per sample
+shifts=5 # number of shifts in vertical height per sample
+batchsize=16 # batch size to load data in
 epochs=1 # number of epochs
 lr=0.0002 # learning rate
 beta1=0.5 # beta 1 for adam optimizer
 beta2=0.999 # beta 2 for adam optimizer
-batchsize=16 # batch size for minibatches
+lambda=100 # weight for L1 loss in generator
+savedir="./images" # directory in which to save the models during training
+width=25 # width of windows
+# Other parameters, such as --lsgan, --subset, --metrics, etc. must be added to the commands below
 
 echo "Beginning training..."
-python -m network.training -r $root -m $model -N $size -s $shifts -w $width -e $epochs -l $lr -b $beta1 $beta2 -B $batchsize -d $savedir -v --force
+python -m network.training -r $root -m $model -N $size -s $shifts -B $batchsize -e $epochs -l $lr -b $beta1 $beta2 --lambda $lambda -d $savedir --force -w $width -v
 echo "Network has finished training"
 echo "Beginning testing..."
-python -m network.testing -r $root -m $model -N $size -s $shifts -w $width -B $batchsize -v -f $savedir/${model}_${epochs}.tar
+python -m network.testing -r $root -m $model -N $size -s $shifts -B $batchsize -f $savedir/${model}_${epochs}.tar -w $width -v
 echo "Network has finished testing"
