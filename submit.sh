5cca03a7910cc287b44aea594f835e8be90b80cc
Lucas Beveridge <lucasbeveridge02@gmail.com>
2023-08-01 13:24:49 +0100

    Small changes to submit.sh

--- submit.sh
+++ submit.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-#SBATCH --job-name=ml_training
-#SBATCH --output=output.log
-#SBATCH --error=error.log
+#SBATCH --job-name=nsn
+#SBATCH --output=training.out
+#SBATCH --error=training.err
 #SBATCH --partition=cs05r
 #SBATCH --gres=gpu:4
 #SBATCH --nodes=4
@@ -20,20 +20,20 @@ echo "MASTER_ADDR="$MASTER_ADDR
 module load python
 conda activate nostripesnet
 
-root="/dls/i12/data/2022/nt33730-1/processing/NoStripesNet/data/pruned" # directory where input data is stored
+root="./data" # directory where input data is stored
 model="patch" # type of model to train. must be one of 'base', 'mask', 'simple', 'patch', 'window' or 'full'
 size=256 # number of sinograms per sample
 shifts=1 # number of shifts in vertical height per sample
-batchsize=32 # batch size to load data in
-epochs=10 # number of epochs
+batchsize=16 # batch size to load data in
+epochs=100 # number of epochs
 lr=0.0002 # learning rate
 beta1=0.5 # beta 1 for adam optimizer
 beta2=0.999 # beta 2 for adam optimizer
 lambda=100 # weight for L1 loss in generator
-savedir="/dls/i12/data/2022/nt33730-1/processing/NoStripesNet/pretrained_models/checkpoints/" # directory in which to save the models during training
+savedir="/path/to/save_directory" # directory in which to save the models during training
 width=25 # width of windows
-name=patch_test
+name="model_name"
 # Other parameters, such as --lsgan, --subset, --metrics, etc. must be added to the commands below
 
 echo "Beginning training..."
-srun python -m network.training -r $root -m $model -N $size -s $shifts -B $batchsize -e $epochs -l $lr -b $beta1 $beta2 --lambda $lambda -d $savedir --force -w $width -v -n $name --ddp
+srun python -m network.training -r $root -m $model -N $size -s $shifts -B $batchsize -e $epochs -l $lr -b $beta1 $beta2 --lambda $lambda -d $savedir --force -w $width -v -n $name --ddp --save-every 25
